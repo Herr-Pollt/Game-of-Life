5010525aeeb34ccd4cef2d9be61e6645444e78c8
Herr-Pollt <124482441+Herr-Pollt@users.noreply.github.com>
2023-07-31 15:20:41 +0800

    finished eating

--- minion.py
+++ minion.py
@@ -12,4 +12,12 @@ class Minion():
               dollars.That would mean that you have {self.balance} dollars.""")
     def eating(self):
         work_efficiency = random.randint(1, 3)
-        print(f"""You have been pretty lazy at work so your hunger got depleted by two""")
+        if work_efficiency == 1:
+            print(f"""You have been pretty lazy at work so your hunger got depleted by two""")
+            self.hunger -= 2
+        elif work_efficiency == 2:
+            print(f"""The boss knows that you have done all your requirements and have been working very hard, so your hunger got depleted by 4""")
+            self.hunger -= 2
+        elif work_efficiency == 3:
+            print(f"""You have been an extremely good worker today and you felt like your really have done a lot of things, so your hunger gets depleted by 6""")
+            self.hunger -= 2
