2a2a251683e2c9dd9d07a1daf9003b28582bef16
Herr-Pollt <124482441+Herr-Pollt@users.noreply.github.com>
2023-08-25 22:12:05 +0800

    made code cleaner

--- main.py
+++ main.py
@@ -4,25 +4,24 @@ from minion import Minion
 def main():
     game = Game()
     for i in range(len(game.players)):
-        name =  game.players[i-1]
+        name = game.players[i].name
         minion = Minion(name)
+        game.players[i] = minion
+
     if game.game_continue:
         game.draw()
         game.gather_players()
-        for i in range(len(game.players)):
+        for player in game.players:
             job = game.pick_job()
-            game.players[i].job = job
+            player.job = job
 
-            #TODO make minions work and eat
-            #TODO When does the game end
-        for i in range(game.round):
-            minion.work()
-            minion.eating()
-            
+        while game.round < 10:
+            for player in game.players:
+                player.work()
+                player.eating()
             game.round += 1
 
-            if game.round == "10":
-                print("game over")
+        print("Game over")
 
 if __name__ == "__main__":
     main()
